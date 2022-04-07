776fbfaa1c1b8052b2aeef5341ff06c78f6f6dc7
Renne Jaskonis <renne.jaskonis@brlink.com.br>
2022-04-07 17:28:04 -0300

    Develop

--- serverless-websocket-and-rest-api/lambda.js
+++ serverless-websocket-and-rest-api/lambda.js
@@ -1,7 +1,24 @@
 const AWS = require("aws-sdk");
 
-async function handler(event) {
+async function handler(event, context, callback) {
     console.log("event: ", JSON.stringify(event, null, 4));
+
+    if (event.httpMethod || (event.requestContext && event.requestContext.http)) {
+        const response = {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+                "Content-Type": "text/html",
+            },
+            // body: JSON.stringify({
+            //     message: "Hello World!!!",
+            //     author: "Renne Jaskonis"
+            // }),
+            body: `<h3>Hello World, Renne Jaskonis!</h3>`,
+        };
+
+        return callback(null, response);
+    }
 
     const {
         requestContext: { domainName, stage, connectionId, routeKey },
